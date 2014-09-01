ece1443a39933ec7e7b802c4fcc878077a3dc316
João Duarte <joao-duarte@users.noreply.github.com>
2014-09-01 18:31:47 +0100

    Update 12.js

--- 12.js
+++ 12.js
@@ -20,9 +20,8 @@ var map = require('through2-map')
 var server = http.createServer(function (req, res) {
   // request handling logic...
   req.pipe(map(function (chunk) {
-  return chunk.toString().toUpperCase()
-  
-})).pipe(res)
-
+   return chunk.toString().toUpperCase()
+  })).pipe(res)
 })
+
 server.listen(process.argv[2])
